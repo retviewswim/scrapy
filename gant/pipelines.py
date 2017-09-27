f8f9e9e141eaaaada8cf8ae64a7d16ba1739c1b6
retviewswim <32062954+retviewswim@users.noreply.github.com>
2017-09-27 07:08:43 +0200

    cleanup up pipelines scrips

--- gant/pipelines.py
+++ gant/pipelines.py
@@ -10,6 +10,7 @@ from gant.items import GantItem, GantColor
 
 class JsonWriterPipeline(object):
     def open_spider(self, spider):
+        """open the item- & color-file"""
         self.item_file = open('items.jl', 'w')
         self.color_file = open('colors.jl', 'w')
 
@@ -18,6 +19,7 @@ class JsonWriterPipeline(object):
         self.color_file.close()
 
     def process_item(self, item, spider):
+        """write item to corresponding file"""
         if isinstance(item, GantItem):
             line = json.dumps(dict(item)) + "\n"
             self.item_file.write(line)
@@ -28,10 +30,13 @@ class JsonWriterPipeline(object):
 
 
 class MergeJsonWriterPipeline(object):
+    """Pipeline that writes the scraped items into 1 file
+       The color-records are added to the items as dictionaries
+    """
     def __init__(self):
-        self.refs_seen = set()
-        self.items = {} # dict of items with key = ref, items = records
-        self.colors = []
+        self.refs_seen = set()  # set that keeps track of the refs seen
+        self.items = {}         # dict of items with key = ref, items = records
+        self.temp_colors = []   # list to temporarily store color records if loaded before item records
 
     def open_spider(self, spider):
         self.items_colors_file = open('items_colors.jl', 'w')
@@ -40,17 +45,24 @@ class MergeJsonWriterPipeline(object):
         self.items_colors_file.close()
 
     def process_item(self, item, spider):
+        # process Items
         if isinstance(item, GantItem):
             ref = item['ref']
-            # put items in dictionary
-            if ref in self.refs_seen:
-                # ignore record if ref already exists
-                pass
-            else:
+            # check if refs were already seen
+            if ref not in self.refs_seen:
                 # add ref to set of seen refs
                 self.refs_seen.add(ref)
                 # add item to dictionary of items
                 self.items[ref] = item
+                # check if ref can be found in temporary color list
+                for color in self.temp_colors:
+                    if ref == color['ref']:
+                        # if found, re-process GantColor item
+                        self.process_item(color)
+            else:
+                pass
+
+        # process Colors
         elif isinstance(item, GantColor):
             color_record = dict(item)
             color = color_record['color']
@@ -59,28 +71,31 @@ class MergeJsonWriterPipeline(object):
                 # add to record if item already exists
                 try:
                     self.items[ref]['details'][color] = color_record
-                except:
+                except IndexError:
                     # if key 'details' doesn't exist, create new
                     self.items[ref]['details'] = {}
                     self.items[ref]['details'][color] = color_record
+
                 # check if item is now complete
-                number_of_colors = len(self.items[ref]['colors'])       # number of colors according to item
-                number_of_color_records = len(self.items[ref]['details'])    # number of color records
+                number_of_colors = len(self.items[ref]['colors'])           # number of colors according to item
+                number_of_color_records = len(self.items[ref]['details'])   # number of color records
+
+                # TO ADD: check if colors are the same
+                colors_items = self.items[ref]['colors']
+                colors_records = self.items[ref]['details']
+
                 # if complete, remove record from dict and write to file
                 if number_of_colors == number_of_color_records:
                     # take full item
                     color_item = self.items[ref]
                     # write to json file
-                    #print("color_item type: ", type(color_item))
-                    #print("color_item dict type: ", type(dict(color_item)))
-                    color_item = dict(color_item)
-                    line = json.dumps(color_item) + "\n"
+                    line = json.dumps(dict(color_item)) + "\n"
                     self.items_colors_file.write(line)
                     # remove from dict & refs_seen
                     del(self.items[ref])
                     self.refs_seen.remove(ref)
             else:
                 # store in temp list if item doesn't exist yet
-                self.colors.append(color_record)
+                self.temp_colors.append(color_record)
 
         return item
